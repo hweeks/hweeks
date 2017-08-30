a74961bd366af1083d3a6bc94b18f598cab41bff
hweeks <h.weeks@gmail.com>
2017-08-30 10:24:14 -0700

    fix all lint issues

--- app/composite/home.js
+++ app/composite/home.js
@@ -4,16 +4,16 @@ import { Link } from 'react-router-dom';
 export default () => (
   <section className="home__container">
     <section className="home__info">
-      I'm a developer and avid <Link className="home__link" to="/bikes">cyclist </Link>
-      in the bay area. I'm currently working at Intuit as a Senior Software Engineer.
+      I&#39;m a developer and avid <Link className="home__link" to="/bikes">cyclist </Link>
+      in the bay area. I&#39;m currently working at Intuit as a Senior Software Engineer.
     </section>
     <section className="home__info">
-        If you'd like to talk, <a href="mailto:root@hweeks.com" className="home__link">
+        If you&#39;d like to talk, <a href="mailto:root@hweeks.com" className="home__link">
           root@hweeks.com
       </a> is best.
     </section>
     <section className="home__info">
-      If you'd like to ride, <a
+      If you&#39;d like to ride, <a
         href="https://www.strava.com/athletes/12027790"
         className="home__link"
         target="_blank"
