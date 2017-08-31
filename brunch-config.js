b082f95fce7500442f3f5f9eecac83f2fc0cbe91
hweeks <h.weeks@gmail.com>
2017-08-30 17:39:57 -0700

    adding stylelint, and building a stylelint plugin for brunch

--- brunch-config.js
+++ brunch-config.js
@@ -4,5 +4,16 @@ exports.files = {
 };
 
 exports.plugins = {
-  babel: {presets: ['latest', 'react']}
+  babel: {presets: ['latest', 'react']},
+  postcss: {
+    options: {
+      parser: require('postcss-scss'),
+    },
+    processors: [
+      require('autoprefixer')(['last 3 versions'])
+    ]
+  },
+  stylelint: {
+    pattern: '^app[\\\/\\\\].*\\\.scss?$'
+  }
 };
